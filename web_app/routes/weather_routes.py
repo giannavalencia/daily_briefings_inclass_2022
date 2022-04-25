b77e1973cd65d09331e20bd8e6ebb159966c8ba3
giannavalencia <gvalealb@gmail.com>
2022-04-25 13:04:21 -0400

    Update weather_routes.py

--- web_app/routes/weather_routes.py
+++ web_app/routes/weather_routes.py
@@ -1,5 +1,3 @@
-# web_app/routes/weather_routes.py
-
 from flask import Blueprint, request, jsonify
 
 from app.weather_service import get_hourly_forecasts
@@ -9,10 +7,12 @@ weather_routes = Blueprint("weather_routes", __name__)
 @weather_routes.route("/api/weather/forecast.json")
 def weather_forecast_api():
     print("WEATHER FORECAST (API)...")
-    print("URL PARAMS:", dict(request.args))
+    
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params)
 
-    country_code = request.args.get("country_code") or "US"
-    zip_code = request.args.get("zip_code") or "20057"
+    country_code = url_params.get("country_code") or "US"
+    zip_code = url_params.get("zip_code") or "20057"
 
     results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
     if results:
